d688942eff543ef6142de206c3be46e06fb75339
kwonyunkyoung <06007sk@gmail.com>
2023-04-13 12:42:40 +0900

    first commit

---
+++ src/main/java/com/skcc/nxcus_dev/NxcusDevApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.nxcus_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxcusDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxcusDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skcc/nxcus_dev/NxcusDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcc.nxcus_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NxcusDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
